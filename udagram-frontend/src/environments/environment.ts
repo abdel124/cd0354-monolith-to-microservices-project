f89674ea526245b215404300c67ac41656f2a730
abdel124 <touazi.abdelhak@gmail.com>
2022-12-18 23:00:29 -0800

    udpates

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5d2b618b7ae14194ae14d527ff95071-531240878.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
